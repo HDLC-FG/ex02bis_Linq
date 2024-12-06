5ff1bafb7172d05298dc6442e5668c15156c1bf5
HDLC <henri.libault-de-la-chevasnerie@fortil.group>
2024-12-06 13:40:58 +0100

    new solution + gitignore

---
+++ ex02bis_Linq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ex02bis_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
